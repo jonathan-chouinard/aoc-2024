cceb2409b843b591841a08e1d4a5f8d9b8d09689
Jonathan Chouinard <jchouinard@dimonoff.com>
2024-12-24 23:08:12 -0500

    feat(day01): part two

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -24,12 +24,26 @@ fn solve_distance(list1: &mut [u32], list2: &mut [u32]) -> u32 {
     list1.iter().zip(list2).map(|(x, y)| x.abs_diff(*y)).sum()
 }
 
+fn solve_similarity(list1: &[u32], list2: &[u32]) -> u32 {
+    let counts = list2.iter().counts();
+    list1
+        .iter()
+        .map(|v| {
+            v * counts.get(v).map_or(0, |x| {
+                u32::try_from(*x).expect("Could not parse usize to u32")
+            })
+        })
+        .sum()
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let input = load_input_file();
     let (mut list1, mut list2) = parse(&input);
     let total_distance = solve_distance(&mut list1, &mut list2);
+    let total_similarity = solve_similarity(&list1, &list2);
 
     println!("Total distance: {}", total_distance);
+    println!("Total similarity: {}", total_similarity);
 
     Ok(())
 }
